af75a5be3a41bb90e1db7cd774187109abe77ff2
Jeffrey Sanford <jeffrey_sanford@hotmail.com>
2026-08-09 19:03:38 -0500

    PR41: enable Kafka retry-topic infrastructure

--- tools/java-ingest/src/main/java/org/phase2/ingest/Application.java
+++ tools/java-ingest/src/main/java/org/phase2/ingest/Application.java
@@ -2,9 +2,11 @@ package org.phase2.ingest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafkaRetryTopic;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@EnableKafkaRetryTopic
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
